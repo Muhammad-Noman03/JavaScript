c5c5d33089f93f3455eb4a0fa59be1bf2af2d049
Muhammad-Noman03 <nomireigns@gmail.com>
2024-05-29 19:33:25 +0500

    added more weather info

--- FetchApi and Async-Await/index.js
+++ FetchApi and Async-Await/index.js
@@ -3,33 +3,53 @@ const input = document.querySelector('#input');
 const searchBtn = document.querySelector('#searchIcon');
 const image = document.querySelector('#weatherImg');
 const city = document.querySelector('#city');
-const temp = document.querySelector('#temp span');
+const temp = document.querySelector('#temp');
 const weatherType = document.querySelector('#weatherType');
+const windSpeed = document.querySelector('#windSpeed span');
+const feelsLike = document.querySelector('#feelsLike span');
+const humidity = document.querySelector('#humidity span');
+const airPressure = document.querySelector('#airPressure span');
 
 // VARIABLES
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const key = "77c49bccf20df0263f4939a2bc0ec27e";
 
-async function weather(city) {
-    const response = await fetch(`${apiURL}${city}&appid=${key}`);
-    const data = await response.json()
-    // console.log(response);
-    // console.log(data);
-    return data;
-}
-
+// INPUT CITY EVENTLISTNER
 input.addEventListener('keydown', async (e) => {
-    let data;
     if (e.key === 'Enter') {
         const value = input.value
-        data = await weather(value);
-        changeWeather(data);
+        weather(value);
     }
 })
 
+// FUNCTION FOR FETCHING WEATHER DATA
+async function weather(city) {
+    try {
+        const response = await fetch(`${apiURL}${city}&appid=${key}`);
+
+        if (!response.ok) {
+            return;
+        }
+
+        const data = await response.json()
+        console.log(data);
+
+        changeWeather(data);
+
+    } catch (error) {
+        console.log(`Data did not fatched --> ${error}`);
+
+    }
+}
+
+// CHANGE WEATHER DISPLAY
 function changeWeather(data) {
     image.src = `openweathermap/${data.weather[0].icon}.svg`
     city.textContent = data.name
-    temp.textContent = Math.round(data.main.temp)
+    temp.textContent = `${Math.round(data.main.temp)}°`
     weatherType.textContent = data.weather[0].main
+    windSpeed.textContent = `${data.wind.speed}Km/h`;
+    feelsLike.textContent = `${data.main.feels_like}°`;
+    humidity.textContent = `${data.main.humidity}`;
+    airPressure.textContent = `${data.main.pressure} atm`
 }
--- FetchApi and Async-Await/tailwind.config.js
+++ FetchApi and Async-Await/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Roboto: ['"Roboto"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
