2eb94021a7c1cefa8fa3e9dcbb72e02780dfb147
Muhammad-Noman03 <nomireigns@gmail.com>
2024-05-30 16:03:28 +0500

    completed Form Validator

--- Task/FormValidator/script.js
+++ Task/FormValidator/script.js
@@ -1,11 +1,38 @@
 // SELECTORS
 const signUpBtn = document.querySelector('#signUpBtn');
 const input = document.querySelector('#input');
+const inputBox = document.querySelector('#inputBox');
 
 // DATA
 const userName = ['Sufiyan', 'Noman', 'Aania'];
 
+// EVENTLISTNER FOR INPUT
 input.addEventListener('input', () => {
+    if (!input.value) {
+        inputBox.classList.remove('found');
+        inputBox.classList.remove('error');
+        return;
+    }
+
     const final = userName.includes(input.value);
-    console.log(final);
+
+    if (final) {
+        inputBox.classList.remove('error');
+        inputBox.classList.add('found');
+        signUpBtn.disabled = !final
+
+    } else {
+        inputBox.classList.remove('found');
+        inputBox.classList.add('error');
+        signUpBtn.disabled = !final
+    }
+
+})
+
+let count = 0;
+
+signUpBtn.addEventListener('click', () => {
+    console.log(count);
+    count++
+
 })
